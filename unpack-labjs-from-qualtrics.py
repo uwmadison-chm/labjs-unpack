7d7ba95687d01f2c205659d65271eaec662fdea0
Dan Fitch <dgfitch@gmail.com>
2020-10-15 15:50:29 -0500

    Cleanup

--- unpack-labjs-from-qualtrics.py
+++ unpack-labjs-from-qualtrics.py
@@ -19,6 +19,9 @@ with open(sys.argv[1], newline='', encoding='utf16') as tsvfile:
     # First two rows out of Qualtrics are header-y stuff, ignore
     next(reader)
     next(reader)
+
+    os.makedirs("output", exist_ok=True)
+
     for row in reader:
         stuff = row['labjs-data']
         if not stuff:
@@ -28,13 +31,12 @@ with open(sys.argv[1], newline='', encoding='utf16') as tsvfile:
             logging.warning("Got row with no labjs data")
         else:
             output_file = os.path.join("output", row['ResponseId'] + '.tsv')
-            os.makedirs("output", exist_ok=True)
+
             # unique set of all possible column names
             columns = set(flatten([x.keys() for x in data]))
 
             with open(output_file, mode='w') as out:
                 writer = csv.writer(out, delimiter='\t')
-
                 writer.writerow(list(columns))
                 for row in data:
                     data = [row.get(col) for col in columns]
